32dd8ccbcf6a0d468cad853f36ecb3bfda0b958d
Erez Bens <erez1158@gmail.com>
2022-03-07 11:15:22 +0200

    added fallback anonymous profile pic

--- frontend/src/Leaderboard.js
+++ frontend/src/Leaderboard.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import "./css/Leaderboard.scss";
+import defaultImg from "./assets/anonymous.jpeg";
 
 const Leaderboard = ({ leaders, currentUser = { uid: "foo" } }) => {
   return (
@@ -7,15 +8,11 @@ const Leaderboard = ({ leaders, currentUser = { uid: "foo" } }) => {
       <Header showBackButton={true} />
       <div className="leaderboard">
         {leaders.map((user, index) => {
+          console.log(user.photoURL);
           return (
-            <div
-              key={user.uid}
-              className={`leaderboard-user-details ${
-                currentUser.uid === user.uid ? "leaderboard-current-user" : ""
-              }`}
-            >
+            <div key={user.uid} className={`leaderboard-user-details ${currentUser.uid === user.uid ? "leaderboard-current-user" : ""}`}>
               <p>#{index + 1}</p>
-              <img src={user.photoURL} alt="" />
+              <img src={user.photoURL || defaultImg} alt="" />
               <div>{user.name}</div>
               <div className="score">{user.points}</div>
             </div>
