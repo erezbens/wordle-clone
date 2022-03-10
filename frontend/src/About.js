50543f94f173fd948138c0a5cea3c64abce56244
Erez Bens <erez1158@gmail.com>
2022-03-10 11:13:05 +0200

    add noopener to href

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -13,12 +13,12 @@ const About = () => {
         </div>
         <div className="links">
           <p>
-            <a href="https://github.com/erezbens/wordle-clone" target="_blank" rel="norefferre noreferrer">
+            <a href="https://github.com/erezbens/wordle-clone" target="_blank" rel="noopener noreferrer">
               Source Code
             </a>
           </p>
           <p>
-            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank" rel="noreferrer">
+            <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank" rel="noopener noreferrer">
               The Original Wordle
             </a>
           </p>
